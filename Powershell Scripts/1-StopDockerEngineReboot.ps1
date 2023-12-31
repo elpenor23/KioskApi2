57ba4801ec62a48365692c3fa328dde1860e8d05
Eric Malinowski <eric.malinowski@gmail.com>
2023-12-31 09:35:27 -0500

    Documenting powershell scripts

--- Powershell Scripts/1-StopDockerEngineReboot.ps1
+++ Powershell Scripts/1-StopDockerEngineReboot.ps1
@@ -1,3 +1,6 @@
+# Docker Daemon and Docker Desktop conflict with each other
+# So we need to stop the Daemon reboot first then we can start 
+# Docker Desktop for building the windows image 
 Set-Service -Name "docker" -Status running -StartupType manual
 Stop-Service -Name "docker"
 Restart-Computer
--- Powershell Scripts/3-LoadStartKiosk.ps1
+++ Powershell Scripts/3-LoadStartKiosk.ps1
@@ -1,7 +1,15 @@
+#Make sure that the docker daemon service is running and 
+#will automatically start 
 Start-Service -Name "docker"
 Set-Service -Name "docker" -Status running -StartupType automatic
 
+#Load out saved image into docker
 Set-Location C:\Users\Eric\Documents\KioskApi2
 docker load -i .\kiosk_api_image.tar
+
+#Create and run a container that will restart automatically
+#defaulting to using 8080 as the port. This can be configured differently
 docker run --restart unless-stopped  -d -p 8080:8080 kioskapi2
+
+#We are done. Bail.
 logoff
