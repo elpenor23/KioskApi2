71e2da90511bc49b8cf04c50b65976cbb2726fa0
Eric Malinowski <eric.malinowski@gmail.com>
2024-01-07 17:06:27 -0500

    Hopefully the last CORS change

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: AllowAllCORS,
         policy =>
         {
-            policy.AllowAnyOrigin().AllowAnyMethod();
+            policy.WithOrigins("*");
         });
 });
 
